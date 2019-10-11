4d410666c3a85c33cd85d97690d797301e934b25
Nadav Tasger <tashernadav@gmail.com>
2019-10-11 14:35:51 +0300

    Progress

--- configuration/setup.php
+++ configuration/setup.php
@@ -8,27 +8,6 @@ const CONFIGURATION_FILE = __DIR__ . DIRECTORY_SEPARATOR . "configuration.json";
 
 $configuration = json_decode(file_get_contents(CONFIGURATION_FILE));
 
-$replacements = [];
-
-
-function uppercase($input)
-{
-    return uppercase($input[0]) . substr($input, 1);
-}
-
-function list_replacements($object = null)
-{
-    global $configuration;
-    if ($object === null)
-        $object = $configuration;
-    $replacements = new stdClass();
-    foreach ($object as $name => $value) {
-        if (is_string($value)) {
-            $replacements->$name = $value;
-        } else {
-            foreach (list_replacements($value) as $nameR => $valueR) {
-//                $replacements->
-            }
-        }
-    }
+foreach ($configuration as $placeholder=>$replacement){
+    // Repindir
 }
